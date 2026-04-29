74b8b5b52b12c3cfef37b5187710af8dd92044ca
Davi Lucas Melo <lucasmelodavi425@gmail.com>
2026-04-29 20:38:16 -0300

    agora vai

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-};
-
-module.exports = nextConfig;
+console.log("Código não é mágica — é prática e persistência 💻🔥");
